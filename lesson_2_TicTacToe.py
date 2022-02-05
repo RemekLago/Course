cc728b47aba12e7099a9ebe20799743368f4eee4
Remigiusz Łagodzki <93308513+RemekLago@users.noreply.github.com>
2022-02-06 00:45:06 +0100

    Add files via upload

--- lesson_2_TicTacToe.py
+++ lesson_2_TicTacToe.py
@@ -20,42 +20,81 @@ Przebieg gry (działanie programu):
 # X - computer
 # O - user
 import random
+from termcolor import colored
 
+def list_board_create():
+    list_board = [".", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    return list_board
 
-def printing_board(list):
-#    list = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-    print(" --- --- ---")
-    print(f"| {list[0]} | {list[1]} | {list[2]} |")
-    print(" --- --- ---")
-    print(f"| {list[3]} | {list[4]} | {list[5]} |")
-    print(" --- --- ---")
-    print(f"| {list[6]} | {list[7]} | {list[8]} |")
-    print(" --- --- ---")
+def printing_board(list_board):
+    print(colored((" --- --- ---"), "yellow"))
+    print(colored((f"| {list_board [1]} | {list_board [2]} | {list_board [3]} |"), "yellow"))
+    print(colored((" --- --- ---"), "yellow"))
+    print(colored((f"| {list_board [4]} | {list_board [5]} | {list_board [6]} |"), "yellow"))
+    print(colored((" --- --- ---"), "yellow"))
+    print(colored((f"| {list_board [7]} | {list_board [8]} | {list_board [9]} |"), "yellow"))
+    print(colored((" --- --- ---"), "yellow"))
+    return list_board
 
-def computer_move():
-    list = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-    start = random.choices([i for i in range(8)])
-    print(start[0])                 #check
-    for id in range(9):
-        if list[start[0]] == "-":
-            list[start[0]] = "X"
+def computer_move(list_board):
+    y = random.choice([i for i in range(1, 10)])
+    if list_board[y] == "-":
+        list_board[y] = "X"
+    elif list_board[y] == "X" or list_board[y] == "O":
+        y = random.choice([i for i in range(1, 10)])
+        while list_board[y] == "X" or list_board[y] == "O":
+            y = random.choice([i for i in range(1, 10)])
+            if list_board[y] == "-":
+                list_board[y] = "X"
 
-    printing_board(list)
-    print(list)                     #check
-    return list
+    print(f"Computer chose: {y}")
+    print(f"The actual list of board: {list_board[1:10]}")
+    printing_board(list_board)
+    return list_board
 
-def user_move():
-    list = computer_move()
-    user = int(input("Your move, enter number for your choice: "))
-    for i in range(len(list)):
-        if list[i] == list[user]:
-            if list[user] == "-":
-                list[user] = "O"
+def human_move(list_board):
+    x = int(input("Please enter your move, choose number (1-9): "))
+    if list_board[x] == "-":
+        list_board[x] = "O"
+    elif list_board[x] == "X" or list_board[x] == "O":
+        print("Please choose another number")
+    else:
+        print("Please choose correct number (1-9)")
+
+    print(f"Human chose: {x}")
+    print(f"The actual list of board: {list_board[1:10]}")
+    printing_board(list_board)
+    return list_board
+
+def check_if_win(list_board):
+    options = [[1, 2, 3], [4, 5, 6], [7, 8, 9],  #rows
+                [1, 4, 7], [2, 5, 8], [3, 6, 9], #columns
+                [1, 5, 9,], [3, 5, 9]]           #cross
+    for i, j in enumerate(options):
+        if list_board[options[i][0]] == list_board[options[i][1]] == list_board[options[i][2]] != "-":
+            if list_board[options[i][0]] == "X":
+                win = "Computer won"
+                print(f"Win: {win}")
+                return win
+                break
+            elif list_board[options[i][0]] == "O":
+                win = "Human won"
+                print(f"Win: {win}")
+                return win
+                break
+            elif "-" not in list_board:
+                win = "R"
+                print(f"Win: {win}")
+                return win
+                break
             else:
                 continue
-        else:
-            print("Please choose other option")
-    printing_board(list)
-    print(list)
 
-user_move()
+def game():
+    x = printing_board(list_board_create())
+    for i in range(1, 10):
+        y = computer_move(x)
+        check_if_win(y)
+        human_move(y)
+        check_if_win(y)
+game()
