170e27097f5d6519e8d7adc7ad9da16c3039a2da
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-02-07 15:52:38 +0100

    correcting definition sequence()

--- lesson_2_sequence_Collatza.py
+++ lesson_2_sequence_Collatza.py
@@ -8,8 +8,7 @@ X może przyjmować wartości od 1 do 100. """
 
 import matplotlib.pyplot as plt
 
-# start_number = input(int("Please enter your number from 1 to 100: "))
-start_number = 90
+start_number = int(input("Please enter your number from 1 to 100: "))
 
 def sequence(start_number):
     list = []
@@ -26,7 +25,6 @@ def sequence(start_number):
             next_number = 3 * number + 1
             number = next_number
             i += 1
-        print(list)                       # for checking is next_number adding correctly
     print(list)
     print(f"Sequence was done in {i} steps.")
     return list
@@ -35,14 +33,10 @@ def sequence(start_number):
 # simple plot of sequence value versus number of iterations
 def plot_of_sequence():
     result = sequence(start_number)
-    x = []
     y = []
-    for id, item in enumerate(result):
-        x.append(id)
-        y.append(item)
-#    print(x)                        # for check
-#    print(y)                        # for check
-    plt.plot(x, y)
+    for id in result:
+        y.append(id)
+    plt.plot(y)
     plt.show()
 
 plot_of_sequence()
