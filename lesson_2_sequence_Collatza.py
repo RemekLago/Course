bb51bace26bcccb8738d8ce12db580ec418ab9af
Remigiusz Łagodzki <93308513+RemekLago@users.noreply.github.com>
2022-02-07 15:57:11 +0100

    Add files via upload

--- lesson_2_sequence_Collatza.py
+++ lesson_2_sequence_Collatza.py
@@ -8,8 +8,7 @@ X może przyjmować wartości od 1 do 100. """
 
 import matplotlib.pyplot as plt
 
-# start_number = input(int("Please enter your number from 1 to 100: "))
-start_number = 90
+start_number = int(input("Please enter your number from 1 to 100: "))
 
 def sequence(start_number):
     list = []
@@ -18,15 +17,12 @@ def sequence(start_number):
     while number > 1:
         if number % 2 == 0:
             list.append(number)
-            next_number = number / 2
-            number = next_number
+            number = number / 2
             i += 1
         else:
             list.append(number)
-            next_number = 3 * number + 1
-            number = next_number
+            number = 3 * number + 1
             i += 1
-        print(list)                       # for checking is next_number adding correctly
     print(list)
     print(f"Sequence was done in {i} steps.")
     return list
@@ -35,14 +31,7 @@ def sequence(start_number):
 # simple plot of sequence value versus number of iterations
 def plot_of_sequence():
     result = sequence(start_number)
-    x = []
-    y = []
-    for id, item in enumerate(result):
-        x.append(id)
-        y.append(item)
-#    print(x)                        # for check
-#    print(y)                        # for check
-    plt.plot(x, y)
+    plt.plot(result)
     plt.show()
 
 plot_of_sequence()
