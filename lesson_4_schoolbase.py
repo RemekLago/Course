6bcb5ea917fb226070e6c04021ac8dbd7a47aa65
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-02-21 09:24:18 +0100

    add method take_input_and_report()

--- lesson_4_schoolbase.py
+++ lesson_4_schoolbase.py
@@ -4,16 +4,16 @@ import sys
 def open_file_with_history():
     """open file to import history of school base
     and split it to list of lists"""
-    command_input = []
+    command_input_history = []
 
     with open("input_lesson_4_schoolbase.txt", "r") as file:
         for row in file:
-            command_input.append(row.strip())
+            command_input_history.append(row.strip())
 
     list_temporary = []
     command_input_final = []
 
-    for idx in command_input:
+    for idx in command_input_history:
         if idx != "" and idx != "koniec":
             list_temporary.append(idx)
         elif idx == "":
@@ -72,54 +72,48 @@ def dictionary_with_staff():
     return dict_of_students, dict_of_teachers
 
 
-# def take_input_and_report():
-#     command_input = sys.argv
-#     if command_input in :
-
-def report_generating_class():
+def report_generating_class(data):
     """when input is name/number of class, function generate report
     with data: students names from this class and tutor's name"""
-    x = "1a"
+    input_command = data
     var = dictionary_with_staff()
     dict_of_students = var[0]
     dict_of_teachers = var[1]
     list_with_results = []
 
     for value in dict_of_teachers.values():
-        if x in value["klasa"] and value["status"] == "wychowawca":
+        if input_command in value["klasa"] and value["status"] == "wychowawca":
             list_with_results.append(value["imie_nazwisko"])
     for value in dict_of_students.values():
-        if x in value["klasa"]:
+        if input_command in value["klasa"]:
             list_with_results.append(value["imie_nazwisko"])
-    print(list_with_results)
     return list_with_results
 
-def report_generating_tutor():
+def report_generating_tutor(data):
     """when input is name of tutor, function generate report
     with data: students names from all tutor's classes"""
 
-    x = "Krzysztof Baczynski"
+    input_command = data
     var = dictionary_with_staff()
     dict_of_students = var[0]
     dict_of_teachers = var[1]
     list_with_results = []
 
     for value in dict_of_teachers.values():
-        if x in value["imie_nazwisko"]:
+        if input_command in value["imie_nazwisko"]:
             list_with_results.append(value["imie_nazwisko"])
             y = value["klasa"]
-    for value in dict_of_students.values():
-        if value["klasa"][0] in y:
-            list_with_results.append(value["imie_nazwisko"])
-    print(list_with_results)
-    return list_with_results
+            for value2 in dict_of_students.values():
+                if value2["klasa"][0] in y:
+                    list_with_results.append(value2["imie_nazwisko"])
+            return list_with_results
 
 
-def report_generating_teacher():
+def report_generating_teacher(data):
     """when input is name of teacher, function generate report
     with data: tutor's names from all classes where teacher teaches"""
 
-    x = "Henryk Sienkiewicz"
+    input_command = data
     var = dictionary_with_staff()
     dict_of_students = var[0]
     dict_of_teachers = var[1]
@@ -127,47 +121,72 @@ def report_generating_teacher():
     set_with_results = set()
 
     for value in dict_of_teachers.values():
-        if x in value["imie_nazwisko"]:
-            list_with_results.append(value["imie_nazwisko"])
+        if input_command in value["imie_nazwisko"]:
+            set_with_results.add(value["imie_nazwisko"])
             y = value["klasa"]
-            print(y)
-    for value in dict_of_teachers.values():
-        if value["status"] == "wychowawca":
-            for idx in value["klasa"]:
+    for value2 in dict_of_teachers.values():
+        if value2["status"] == "wychowawca":
+            for idx in value2["klasa"]:
                 if idx in y:
-                    set_with_results.add(value["imie_nazwisko"])
-    for idx in set_with_results:
-        list_with_results.append(idx)
-
-#    print(list_with_results)
+                    set_with_results.add(value2["imie_nazwisko"])
+    for idy in set_with_results:
+        list_with_results.append(idy)
     return list_with_results
 
 
-def report_generating_student():
+def report_generating_student(data):
     """when input is name of student, function generate report
     with data:names of subjects which student learners and
     teacher's names of that subjects"""
 
-    x = "Zygmunt Krasiński"
+    input_command = data
     var = dictionary_with_staff()
     dict_of_students = var[0]
     dict_of_teachers = var[1]
     list_with_results = []
-    set_with_results = set()
 
     for value in dict_of_students.values():
-        if x in value["imie_nazwisko"]:
+        if input_command in value["imie_nazwisko"]:
             list_with_results.append(value["imie_nazwisko"])
             y = value["klasa"]
-            print(y)
-    for value in dict_of_teachers.values():
-        if value["status"] == "nauczyciel":
-            for idx in value["klasa"]:
-                if idx in y:
-                    list_with_results.append(value["przedmiot"])
-                    list_with_results.append(value["imie_nazwisko"])
-
-    print(list_with_results)
+            for value in dict_of_teachers.values():
+                if value["status"] == "nauczyciel":
+                    for idx in value["klasa"]:
+                        if idx in y:
+                            list_with_results.append(value["przedmiot"])
+                            list_with_results.append(value["imie_nazwisko"])
     return list_with_results
 
-report_generating_student()
+
+def take_input_and_report():
+    """it takes input and find what input means: class, student, teacher
+    or tutor. Then start the right method and write it to output file"""
+
+    x = sys.argv
+    command_input = x[1:][0]
+    var = dictionary_with_staff()
+    dict_of_students = var[0]
+    dict_of_teachers = var[1]
+
+    for value in dict_of_teachers.values():
+        if command_input in value["klasa"] and value["status"] == "wychowawca":
+            i = report_generating_class(command_input)
+            return i
+        if command_input in value["imie_nazwisko"] and value["status"] == "wychowawca":
+            i = report_generating_tutor(command_input)
+            return i
+        if command_input in value["imie_nazwisko"] and value["status"] == "nauczyciel":
+            i = report_generating_teacher(command_input)
+            return i
+    for value in dict_of_students.values():
+        if command_input in value["imie_nazwisko"]:
+            i = report_generating_student(command_input)
+            return i
+
+
+with open("output_lesson_4_schoolbase.txt", "w") as file:
+    i = take_input_and_report()
+    for element in i:
+        file.write(element + "\n")
+
+take_input_and_report()
