d6b55303d67dc974dbe34f7361632a948638f8a6
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-02-22 20:17:17 +0100

    changes in loops' conditions - checking balance account level before act

--- lesson_3_accountant.py
+++ lesson_3_accountant.py
@@ -2,7 +2,6 @@ import sys
 from termcolor import colored
 from input_accountant import command_final
 
-print(command_final)
 history_account = []
 balance_start = 0
 product_dict = {}
@@ -15,51 +14,53 @@ new_command_input = sys.argv
 command_final.append(new_command_input[1:])
 command_final.append(command_stop)
 
-print(f"Command final: \n {command_final}")
-
 "loop with all 'if' and adding operation to list with history"
 for data_input in command_final:
     if data_input[0] == "saldo":
         balance = int(data_input[1])
         comment = data_input[2]
         history_account.append(data_input)
-        balance_start += balance
+        if balance_start + balance < 0:
+            print("ERROR - not enough money on account")
+        else:
+            balance_start += balance
 
     elif data_input[0] == "zakup":
         product_id = data_input[1]
         price = int(data_input[2])
         quantity = int(data_input[3])
-        balance_start -= price * quantity
-        if product_dict.get(f"{product_id}"):
-            product_dict[product_id] = product_dict[product_id] + quantity
-        elif not product_dict.get(f"{product_id}"):
-            product_dict.update({product_id: quantity})
-            print(f"Zakup: {product_id} {quantity}")
-            print(product_dict)
-        elif balance_start < 0:     # or balance_start < price * quantity
-            print("ERROR - balance lower than 0")
+        if balance_start - price * quantity < 0:
+            print("ERROR - not enough money on account")
         elif price < 0:
             print("ERROR - price lower than 0")
         elif quantity < 0:
             print("ERROR - quantity lower than 0")
-        history_account.append(data_input)
+        else:
+            balance_start -= price * quantity
+            if product_dict.get(f"{product_id}"):
+                product_dict[product_id] = product_dict[product_id] + quantity
+            elif not product_dict.get(f"{product_id}"):
+                product_dict.update({product_id: quantity})
+#                print(f"Zakup: {product_id} {quantity}")
+#                print(product_dict)
+            history_account.append(data_input)
 
     elif data_input[0] == "sprzedaz":
         product_id = data_input[1]
         price = int(data_input[2])
         quantity = int(data_input[3])
         balance_start += price * quantity
-        if product_dict.get(f"{product_id}") >= quantity:
-            product_dict[product_id] -= quantity
-        elif product_dict.get(f"{product_id}") < quantity:
+        if product_dict.get(f"{product_id}") < quantity:
             print("ERROR - quantity after sale lower than 0")
-        if price < 0:
+        elif price < 0:
             print("ERROR - price lower than 0")
-        if quantity < 0:
+        elif quantity < 0:
             print("ERROR - quantity lower than 0")
+        elif product_dict.get(f"{product_id}") >= quantity:
+            product_dict[product_id] -= quantity
         history_account.append(data_input)
-        print(f"Sprzedaz: {product_id} {product_dict[product_id]}")
-        print(product_dict)
+#        print(f"Sprzedaz: {product_id} {product_dict[product_id]}")
+#        print(product_dict)
 
     elif data_input[0] == "konto":
         print(f"{balance_start}")
