1fdfd8581e205e46bf181ae27ef8d8ef3f2a8695
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-27 19:15:12 +0200

    changes in function is_destination_path_exist

--- lesson_6_reader.py
+++ lesson_6_reader.py
@@ -5,9 +5,10 @@ import pickle
 import sys
 
 # x = sys.argv
-x = ["lesson_6_reader.py", "TestDict/bazatest.csv", "TestDict/TestDict4/bazatest4.json", "1,1,2", "1,2,6"]
+x = ["lesson_6_reader.py", "TestDict/bazatest.csv",
+     "TestDict/TestDict1/bazatest5.json", "1,1,2", "1,1,6"]
 changes = [[int(idy) for idy in idx.split(",")] for idx in x[3:]]
-print(changes)
+
 
 class CommandData:
     def __init__(self, src, dst, changes):
@@ -16,7 +17,8 @@ class CommandData:
         self.changes = changes
         self.input_type = self.type_of_file(src)
         self.output_type = self.type_of_file(dst)
-
+        self.source_path_exist = self.is_source_path_exist()
+        self.destination_path_exist = self.is_destination_path_exist()
 
     def file_csv_read(self):
         with open(self.src, "r") as file:
@@ -53,10 +55,8 @@ class CommandData:
             pickle.dump(file_with_changes, file)
 
     def type_of_file(self, path):
-        # path = sys.argv[1]
         type_file = None
         if path.endswith(".json"):
-            # type_of_file = json
             type_file = "json"
         elif path.endswith(".csv"):
             type_file = "csv"
@@ -66,7 +66,22 @@ class CommandData:
             print("ERROR, given file extension isn't json, csv or pickle")
         return type_file
 
+    def is_source_path_exist(self):
+        path = os.path.exists(self.src)
+        if path:
+            return True
+        else:
+            print("ERROR, given path doesn't exist")
+
+    def is_destination_path_exist(self):
+        path_folder = os.path.split(self.dst)
+        path = os.path.exists(path_folder[0])
+        if not path:
+            os.makedirs(path_folder[0])
+
     def process(self):
+        self.is_source_path_exist()
+        self.is_destination_path_exist()
         if self.input_type == "csv":
             content = self.file_csv_read()
         elif self.input_type == "json":
@@ -85,53 +100,6 @@ class CommandData:
 command = CommandData(x[1], x[2], changes)
 
 command.process()
-
-
-#
-# def is_source_path_exist(src):
-#     path = os.path.exists(src)
-#     if path:
-#         return True
-#     else:
-#         print("ERROR, given path doesn't exist")
-#
-#
-# def is_destination_path_exist(dst):
-#     path_folder = os.path.split(dst)
-#     path = os.path.exists(path_folder[1])
-#     if not path:
-#         os.makedirs(path_folder[1])
-#
-#
-# def take_reader_to_edit():
-#     source = is_source_path_exist()
-#     type_input = type_of_input_file()
-#     if type_input == "csv" and source:
-#         file_to_edit = command.file_csv_read(x[1])
-#     elif type_input == "json" and source:
-#         file_to_edit = command.file_json_read(x[1])
-#     elif type_input == "pickle" and source:
-#         file_to_edit = command.file_pickle_read(x[1])
-#     return file_to_edit
-#
-# def take_file_to_write():
-#     type_output = type_of_output_file()
-#     if type_output == "csv":
-#         command.file_csv_write(x[1])
-#     elif type_output == "json":
-#         command.file_json_write(x[1])
-#     elif type_output == "pickle":
-#         command.file_pickle_write(x[1])
-#
-# def final():
-#     is_destination_path_exist()
-#     # type_of_input_file()
-#     type_of_output_file()
-#     file_to_edit = take_reader_to_edit()
-#     command.changes_in_file(changes, file_to_edit)
-#     take_file_to_write()
-#
-# # final()
 
 
 
